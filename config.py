6364de4f3f11d25ad05ca249c259e8bbe6980f12
nobody7910 <unknown12345hacker321@gmail.com>
2025-01-29 01:38:32 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,114 @@
+import re
+from os import getenv
+
+from dotenv import load_dotenv
+from pyrogram import filters
+
+load_dotenv()
+
+# Get this value from my.telegram.org/apps
+API_ID = 23882380
+API_HASH = "717f0a9521573f91562c2b3bd38f0b3c"
+
+# Get your token from @BotFather on Telegram.
+BOT_TOKEN = "7612831056:AAH86ofYIVH5-OE0sTrhvW2F1YCCk_etDFM"
+
+# Get your mongo url from cloud.mongodb.com
+MONGO_DB_URI = "mongodb+srv://nobodyknowns928:nobodyknowns928@nobody0.5zwwq.mongodb.net/?retryWrites=true&w=majority&appName=Nobody0"
+
+DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 60))
+
+# Chat id of a group for logging bot's activities
+LOG_GROUP_ID = -1002081085931
+
+# Get this value from @ultron2_robot on Telegram by /id
+OWNER_ID = 5947904339
+
+## Fill these variables if you're deploying on heroku.
+# Your heroku app name
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
+# Get it from http://dashboard.heroku.com/account
+HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+
+UPSTREAM_REPO = getenv(
+    "UPSTREAM_REPO",
+    "https://github.com/rishabhops/alice",
+)
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
+GIT_TOKEN = getenv(
+    "GIT_TOKEN", None
+)  # Fill this variable if your upstream repository is private
+
+SUPPORT_CHANNEL = "https://t.me/AREA_OF_friends_477"
+SUPPORT_GROUP = "https://t.me/SWAGAT_HAI_TUMHARA_791"
+
+# Set this to True if you want the assistant to automatically leave chats after an interval
+AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
+
+
+# Get this credentials from https://developer.spotify.com/dashboard
+SPOTIFY_CLIENT_ID = getenv("SPOTIFY_CLIENT_ID", None)
+SPOTIFY_CLIENT_SECRET = getenv("SPOTIFY_CLIENT_SECRET", None)
+
+
+# Maximum limit for fetching playlist's track from youtube, spotify, apple links.
+PLAYLIST_FETCH_LIMIT = int(getenv("PLAYLIST_FETCH_LIMIT", 25))
+
+
+# Telegram audio and video file size limit (in bytes)
+TG_AUDIO_FILESIZE_LIMIT = int(getenv("TG_AUDIO_FILESIZE_LIMIT", 104857600))
+TG_VIDEO_FILESIZE_LIMIT = int(getenv("TG_VIDEO_FILESIZE_LIMIT", 2145386496))
+# Checkout https://www.gbmb.org/mb-to-bytes for converting mb to bytes
+
+
+# Get your pyrogram v2 session from Replit
+STRING1 = "BQFsaowAqToivAsIdWF7SR_eTcwGmk1NiOmoOZyfkBsgv94MCrjuGJWMPdmSY3TpoMJcl327YGeMotW63rSlatyHvC8SceX9z3EwtnbqF5L6scXKPaZF_FImO0paa52LqRsJ189ezJ9DHUZJCO0ga2Ddj79SvFPEJlCubvBjEVHw2CGyhwgKZK0G8zuhmBUSzaBOXa3xO1FJAobEkBIDOKZoBIT9r6NInl7ZI7XIfFfqZof-WAcmfwrEYWmsQY87f7r-qhH1MTMfFtOaqqucWv0-u43TC3SpB8yXGNh9UANyKN2DmrSl4PgcNsz1guCXOAegc6woxDgmZYCWw7bicsLiogaAbwAAAAHfooQnAA"
+STRING2 = getenv("STRING_SESSION2", None)
+STRING3 = getenv("STRING_SESSION3", None)
+STRING4 = getenv("STRING_SESSION4", None)
+STRING5 = getenv("STRING_SESSION5", None)
+
+
+BANNED_USERS = filters.user()
+adminlist = {}
+lyrical = {}
+votemode = {}
+autoclean = []
+confirmer = {}
+
+
+START_IMG_URL = "https://graph.org/file/0e5027f29d8a71f3c56ac-e14b0f373dd9b5b75c.jpg"
+
+PING_IMG_URL = "https://graph.org/file/0e5027f29d8a71f3c56ac-e14b0f373dd9b5b75c.jpg"
+
+PLAYLIST_IMG_URL = "https://graph.org/file/763a841a2ad5cbb1e2fc5.jpg"
+STATS_IMG_URL = "https://graph.org/file/f586172fe40a0b5d0b0df.jpg"
+TELEGRAM_AUDIO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
+TELEGRAM_VIDEO_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
+STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
+SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
+YOUTUBE_IMG_URL = "https://graph.org//file/2f7debf856695e0ef0607.png"
+SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"
+
+
+def time_to_seconds(time):
+    stringt = str(time)
+    return sum(int(x) * 60**i for i, x in enumerate(reversed(stringt.split(":"))))
+
+
+DURATION_LIMIT = int(time_to_seconds(f"{DURATION_LIMIT_MIN}:00"))
+
+
+if SUPPORT_CHANNEL:
+    if not re.match("(?:http|https)://", SUPPORT_CHANNEL):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_CHANNEL url is wrong. Please ensure that it starts with https://"
+        )
+
+if SUPPORT_GROUP:
+    if not re.match("(?:http|https)://", SUPPORT_GROUP):
+        raise SystemExit(
+            "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
+        )
